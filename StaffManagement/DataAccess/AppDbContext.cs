16d2910494486824e21c67d4e4188b09894e6bff
Ewelike Emeka Nelson <emekaewelike@gmail.com>
2022-04-08 13:45:03 +0100

    created the migrations for our staffs

---
+++ StaffManagement/DataAccess/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StaffManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StaffManagement.DataAccess
+{
+    public class AppDbContext : IdentityDbContext<Staff>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Staff> Staffs { get; set; }
+    }
+}
