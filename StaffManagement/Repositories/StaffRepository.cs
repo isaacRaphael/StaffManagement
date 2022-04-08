9126a875b3b63c073a5a3a6b8f1586445fc68f37
Uzor Nwachukwu <uzor.nwachukwu@thebulbafrica.institute>
2022-04-08 14:48:12 +0100

    I added the implementation of the StaffRepository

---
+++ StaffManagement/Repositories/StaffRepository.cs
@@ -0,0 +1,48 @@
+﻿using StaffManagement.Contracts;
+using StaffManagement.DataAccess;
+using StaffManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Repositories
+{
+    public class StaffRepository : IStaffRepository
+    {
+        private readonly AppDbContext _db;
+
+        public StaffRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public bool AddStaff(Staff staff)
+        {
+            _db.Staffs.Add(staff);
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+        public IEnumerable<Staff> GetAllStaff()
+        {
+            return _db.Staffs.ToList();
+        }
+
+        public Staff GetStaff(int id)
+        {
+            return _db.Staffs.Find(id);
+        }
+
+        public bool RemoveStaff(int id)
+        {
+            var staff = _db.Staffs.Find(id);
+            if(staff == null)
+            {
+                return false;            
+            }
+
+            _db.Staffs.Remove(staff);
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+    }
+}
