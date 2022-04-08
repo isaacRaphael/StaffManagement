7fa9ffbfd29662fa49a0cea3d335b50ac80e696c
Raphael Isaac <isaacraphaelchidi@gmail.com>
2022-04-08 14:07:40 +0100

    Created Staff Repository

--- StaffManagement/Contracts/IStaffRepository.cs
+++ StaffManagement/Contracts/IStaffRepository.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using StaffManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StaffManagement.Contracts
 {
-    interface Interface
+    public interface IStaffRepository
     {
+        bool AddStaff(Staff staff);
+        bool RemoveStaff(int id);
+        Staff GetStaff(int id);
+        IEnumerable<Staff> GetAllStaff();
     }
 }
