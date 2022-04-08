399135b8dd11cf840ed4495f37887c3127cb3653
Raphael Isaac <isaacraphaelchidi@gmail.com>
2022-04-08 13:04:59 +0100

    added a staff model and added nugget packages for Identityand Ef

---
+++ StaffManagement/Models/Staff.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Models
+{
+    public class Staff : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string UserName { get; set; }
+        public string JobTitle { get; set; }
+        public string Department { get; set; }
+
+    }
+}
+
+
+
